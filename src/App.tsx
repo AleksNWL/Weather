7889420b5f142708d2221d9afb8e19d1a02b3583
Aleksey Nemesh <Grifon595@yandex.ru>
2025-05-05 22:09:53 +0300

    Added global colors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
+import './App.scss'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home/Home.tsx"
 
 
 export default function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-            </Routes>
-        </Router>
-    )
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
+  )
 }
